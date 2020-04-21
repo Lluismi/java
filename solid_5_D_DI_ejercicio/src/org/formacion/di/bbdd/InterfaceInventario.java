ac0107ade085c6a8e4be1e9a8e8f0c91df9646a7
Lluismi <lmorey@cifpfbmoll.eu>
2020-04-21 02:21:07 +0200

    Creada interfaz inventario, y implementada interfaz a la clase

---
+++ solid_5_D_DI_ejercicio/src/org/formacion/di/bbdd/InterfaceInventario.java
@@ -0,0 +1,6 @@
+package org.formacion.di.bbdd;
+
+public interface InterfaceInventario {
+	
+	public int numeroProductos(String tienda, String producto);
+}
--- solid_5_D_DI_ejercicio/src/org/formacion/di/bbdd/InventarioBBDD.java
+++ solid_5_D_DI_ejercicio/src/org/formacion/di/bbdd/InventarioBBDD.java
@@ -1,12 +1,14 @@
 package org.formacion.di.bbdd;
 
 
-public class InventarioBBDD {
+public class InventarioBBDD implements InterfaceInventario {
 
 
 	// Metodo de consulta a la base de datos
 	
+	@Override
 	public int numeroProductos(String tienda, String producto) {
 		return BBDD.stocs.get(tienda).get(producto);
 	}
+
 }
