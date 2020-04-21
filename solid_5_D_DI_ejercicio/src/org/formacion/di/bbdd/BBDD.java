197494fe65e0795b0e6c36a22958f595ea98d3f6
Lluismi <lmorey@cifpfbmoll.eu>
2020-04-21 02:22:21 +0200

    refactor visibilidad

--- solid_5_D_DI_ejercicio/src/org/formacion/di/bbdd/BBDD.java
+++ solid_5_D_DI_ejercicio/src/org/formacion/di/bbdd/BBDD.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class BBDD {
 
 	// ----- esta es nuestra "base de datos" -----
-	public static Map<String, Map<String, Integer>> stocs = new HashMap<>();
+	static Map<String, Map<String, Integer>> stocs = new HashMap<>();
 	
 	static {
 		Map<String, Integer> stocTiendaNorte = new HashMap<>();
